8ad78f478c445e3b884a50ca3acafc5ceb62c8a6
J3eff <jeffersonvideo125@gmail.com>
2025-07-21 23:54:30 -0300

    feat:  Instalando e configurando o JSON server

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface IUsuario {
+  id: number;
+  nome: string;
+  renda: number;
+}
+
+export interface ITransacao {
+    id: number;
+    nome: string;
+    valor: number;
+    tipo: "receita" | "despesa";
+    categoria: string;
+    data: string;
+}
