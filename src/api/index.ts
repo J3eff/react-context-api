faa9613ce0eda91e6c4cf2642ffc8eebb73eb41c
J3eff <jeffersonvideo125@gmail.com>
2025-07-22 00:04:12 -0300

    feat: Criando funções para buscar e adicionar usuário

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { IUsuario } from "../types";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "http://localhost:5000",
 });
+
+export const obterUsuario = async (): Promise<IUsuario[]> => {
+  const { data } = await api.get<IUsuario[]>("/usuarios");
+  return data;
+};
+
+export const criaUsuario = async (usuario: Omit<IUsuario, "id">): Promise<IUsuario> => {
+  const { data } = await api.post<IUsuario>("/usuarios", usuario);
+  return data;
+};
